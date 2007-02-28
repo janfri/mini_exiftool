6f71e17ad6191219d3f85b77966890bedaf97143
Jan Friedrich <janfri@hermon.(none)>
2007-02-28 21:19:54 +0100

    Reorganising tests

--- test/test_read.rb
+++ test/test_read.rb
@@ -11,7 +11,6 @@ class TestRead < Test::Unit::TestCase
     @data_dir = File.dirname(__FILE__) + '/data'
     @filename_test = @data_dir + '/test.jpg'
     @mini_exiftool = MiniExiftool.new @filename_test
-    @mini_exiftool_num = MiniExiftool.new @filename_test, :numerical => true
   end
 
   def test_access
@@ -21,15 +20,8 @@ class TestRead < Test::Unit::TestCase
     assert_equal 400, @mini_exiftool.iso
   end
 
-  def test_access_numerical
-    assert_equal 'DYNAX 7D', @mini_exiftool_num['Model']
-    assert_equal 'MLT0', @mini_exiftool_num['maker_note_version']
-    assert_equal 'MLT0', @mini_exiftool_num.maker_note_version
-    assert_equal 400, @mini_exiftool_num.iso
-  end
-
   def test_tags
-    assert @mini_exiftool_num.tags.include?('FileSize')
+    assert @mini_exiftool.tags.include?('FileSize')
   end
 
   def test_conversion
@@ -39,16 +31,6 @@ class TestRead < Test::Unit::TestCase
     assert_kind_of String, @mini_exiftool.flash
     assert_kind_of Fixnum, @mini_exiftool['ExposureCompensation']
     assert_kind_of Array, @mini_exiftool['SubjectLocation']
-  end
-
-  def test_conversion_numerical
-    assert_kind_of String, @mini_exiftool_num.model
-    assert_kind_of Time, @mini_exiftool_num['DateTimeOriginal']
-    assert_kind_of Float, @mini_exiftool_num['MaxApertureValue']
-    assert_kind_of Fixnum, @mini_exiftool_num.flash
-    assert_kind_of String, @mini_exiftool_num.exif_version
-    assert_kind_of Fixnum, @mini_exiftool_num['ExposureCompensation']
-    assert_kind_of Array, @mini_exiftool_num['SubjectLocation']
   end
 
 end
---
+++ test/test_read_numerical.rb
@@ -0,0 +1,33 @@
+require 'mini_exiftool'
+require 'test/unit'
+begin
+  require 'turn'
+rescue LoadError
+end
+
+class TestReadNumerical < Test::Unit::TestCase
+
+  def setup
+    @data_dir = File.dirname(__FILE__) + '/data'
+    @filename_test = @data_dir + '/test.jpg'
+    @mini_exiftool_num = MiniExiftool.new @filename_test, :numerical => true
+  end
+
+  def test_access_numerical
+    assert_equal 'DYNAX 7D', @mini_exiftool_num['Model']
+    assert_equal 'MLT0', @mini_exiftool_num['maker_note_version']
+    assert_equal 'MLT0', @mini_exiftool_num.maker_note_version
+    assert_equal 400, @mini_exiftool_num.iso
+  end
+
+  def test_conversion_numerical
+    assert_kind_of String, @mini_exiftool_num.model
+    assert_kind_of Time, @mini_exiftool_num['DateTimeOriginal']
+    assert_kind_of Float, @mini_exiftool_num['MaxApertureValue']
+    assert_kind_of Fixnum, @mini_exiftool_num.flash
+    assert_kind_of String, @mini_exiftool_num.exif_version
+    assert_kind_of Fixnum, @mini_exiftool_num['ExposureCompensation']
+    assert_kind_of Array, @mini_exiftool_num['SubjectLocation']
+  end
+
+end
