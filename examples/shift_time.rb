104a2b36d0b0aecb68b567e8750812d7486a5da1
Jan Friedrich <janfri@ubuntu.localdomain>
2007-03-14 14:37:38 +0100

    New example shift_time

---
+++ examples/shift_time.rb
@@ -0,0 +1,29 @@
+require 'rubygems'
+require 'mini_exiftool'
+
+if ARGV.size < 2
+  puts "usage: ruby #{__FILE__} [+|-]SECONDS FILES"
+  puts " i.e.: ruby #{__FILE__} 3600 *.jpg"
+  exit -1
+end
+
+delta = ARGV.shift.to_i
+
+ARGV.each do |filename|
+  begin
+    photo = MiniExiftool.new filename
+  rescue MiniExiftool::Error => e
+    $stderr.puts e.message
+    exit -1
+  end
+  time = photo.date_time_original
+  # time is now a Time object, so we can use the methodos of it :)
+  time += delta
+  photo.date_time_original = time
+  save_ok = photo.save
+  if save_ok
+    puts "#{filename} changed"
+  else
+    puts "#{filename} could not be changed"
+  end
+end
