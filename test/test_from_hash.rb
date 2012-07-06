73e8ceff24b438973aaf4e1fe908b7ce06df198a
Ethan Soutar-Rau <ethan.soutar.rau@gmail.com>
2012-07-05 21:14:23 -0700

    from_hash applies type conversion to values
    The from_hash method accepted hashes without providing
    any of the useful type conversions that MiniExiftool performs when
    it loads files.

---
+++ test/test_from_hash.rb
@@ -0,0 +1,22 @@
+require 'helpers_for_test'
+require 'json'
+
+class TestFromHash < TestCase
+  def setup
+    @data_dir = File.dirname(__FILE__) + '/data'
+    hash_data = JSON.parse(File.read( @data_dir + '/test.jpg.json')).first
+    @mini_exiftool = MiniExifTool.from_hash hash_data
+  end
+
+  def test_conversion
+    assert_kind_of String, @mini_exiftool.model
+    assert_kind_of Time, @mini_exiftool['DateTimeOriginal']
+    assert_kind_of Float, @mini_exiftool['MaxApertureValue']
+    assert_kind_of String, @mini_exiftool.flash
+    assert_kind_of Fixnum, @mini_exiftool['ExposureCompensation']
+    assert_kind_of String, (@mini_exiftool['SubjectLocation'] || @mini_exiftool['SubjectArea'])
+    assert_kind_of Array, @mini_exiftool['Keywords']
+    assert_kind_of String, @mini_exiftool['SupplementalCategories']
+    assert_kind_of Rational, @mini_exiftool.shutterspeed
+  end
+end
