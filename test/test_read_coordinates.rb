1030b9ef5a2f13719c7ef7b03d6356ce3bbc137b
Jan Friedrich <janfri26@gmail.com>
2013-05-30 21:52:07 +0200

    Change test_access_coordinates again to support different exiftool outputs.

--- test/test_read_coordinates.rb
+++ test/test_read_coordinates.rb
@@ -9,11 +9,10 @@ class TestReadCoordinates < TestCase
   end
 
   def test_access_coordinates
-    mini_exiftool_coord = MiniExiftool.new @filename_test, :coord_format => "%+.6f degrees"
-    # checking float equality is here ok ;-)
-    assert_match /^\+43.653167 degrees/, mini_exiftool_coord['GPSLatitude']
-    assert_match /^-79.373167 degrees/, mini_exiftool_coord['GPSLongitude']
-    assert_match /^\+43.653167 degrees.*, -79.373167 degrees/, mini_exiftool_coord['GPSPosition']
+    mini_exiftool_coord = MiniExiftool.new @filename_test, :coord_format => "%.6f degrees"
+    assert_match /^43.653167 degrees/, mini_exiftool_coord['GPSLatitude']
+    assert_match /^79.373167 degrees/, mini_exiftool_coord['GPSLongitude']
+    assert_match /^43.653167 degrees.*, 79.373167 degrees/, mini_exiftool_coord['GPSPosition']
   end
 
 end
