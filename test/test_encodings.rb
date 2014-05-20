a9e1fd3bb78de4858f131daf1ae50401f263e95b
Jan Friedrich <janfri26@gmail.com>
2014-05-20 21:28:29 +0200

    New test for CodedCharacterSet.

--- test/test_encodings.rb
+++ test/test_encodings.rb
@@ -3,17 +3,41 @@ require 'helpers_for_test'
 
 class TestEncodings < TestCase
 
+  include TempfileTest
+
   def setup
+    super
     @data_dir = File.dirname(__FILE__) + '/data'
     @filename_test = @data_dir + '/test_encodings.jpg'
     @mini_exiftool = MiniExiftool.new @filename_test
+    @object_name = "Möhre"
   end
 
   def test_iptc_encoding
-    object_name = "Möhre"
-    assert_not_equal object_name, @mini_exiftool.object_name
+    assert_not_equal @object_name, @mini_exiftool.object_name
     correct_iptc = MiniExiftool.new(@filename_test, iptc_encoding: 'MacRoman')
-    assert_equal object_name, correct_iptc.object_name
+    assert_equal @object_name, correct_iptc.object_name
+  end
+
+  def test_coded_character_set
+    assert_nil @mini_exiftool.coded_character_set
+
+    FileUtils.cp(@filename_test, @temp_filename)
+    @mini_exiftool2 = MiniExiftool.new @temp_filename
+    @mini_exiftool2.coded_character_set = 'utf8'
+    caption = 'Das ist eine Möhre'
+    @mini_exiftool2.caption_abstract = caption
+    @mini_exiftool2.save!
+    @mini_exiftool2.reload
+    assert_equal 'UTF8', @mini_exiftool2.coded_character_set
+    assert_equal caption, @mini_exiftool2.caption_abstract
+    assert_not_equal @object_name, @mini_exiftool2.object_name
+
+    @mini_exiftool2.object_name = @object_name
+    @mini_exiftool2.save!
+    @mini_exiftool2.reload
+    assert_equal caption, @mini_exiftool2.caption_abstract
+    assert_equal @object_name, @mini_exiftool2.object_name
   end
 
 end
