f8aa10a82add4c413f92a92610d5b7e2a9b9f544
Jan Friedrich <janfri26@gmail.com>
2016-01-14 12:34:59 +0100

    Don't ignore ExifToolVersion in regtest samples.

--- regtest/read_all.rb
+++ regtest/read_all.rb
@@ -4,7 +4,7 @@ require 'regtest'
 Dir['test/data/*.jpg'].sort.each do |fn|
   Regtest.sample 'read ' << File.basename(fn) do
     h = MiniExiftool.new(fn).to_hash
-    %w(ExifToolVersion FileModifyDate FileAccessDate FileInodeChangeDate FilePermissions).each do |tag|
+    %w(FileModifyDate FileAccessDate FileInodeChangeDate FilePermissions).each do |tag|
       h.delete(tag)
     end
     h
