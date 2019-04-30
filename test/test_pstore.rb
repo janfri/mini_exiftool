272d2071e53dc8fb1a9720c3a3dd79544e1b1c6c
Jan Friedrich <janfri26@gmail.com>
2019-04-30 19:07:47 +0200

    Eliminate startup time in PStore test
    The PStore tests are executed as external process. So in cases with high
    startup time (i.e. when running on JRuby) it blanketed the speedup when
    reading cached tag information and the test failed.

--- test/test_pstore.rb
+++ test/test_pstore.rb
@@ -5,20 +5,33 @@ class TestPstore < TestCase
 
   def test_pstore
     pstore_dir = Dir.mktmpdir
-    s = MiniExiftool.writable_tags.size.to_s
-    cmd = %Q(#{RUBY_ENGINE} -EUTF-8 -I lib -r mini_exiftool -e "MiniExiftool.pstore_dir = '#{pstore_dir}'; p MiniExiftool.writable_tags.size")
-    a = Time.now
-    result = `#{cmd}`
-    b = Time.now
-    assert_equal s, result.chomp
+    writable_tags = MiniExiftool.writable_tags
+    res = execute(pstore_dir)
+    t1 = res['time']
+    assert_equal writable_tags, res['writable_tags']
     assert_equal 1, Dir[File.join(pstore_dir, '*')].size
-    c = Time.now
-    result = `#{cmd}`
-    d = Time.now
-    assert_equal s, result.chomp
-    assert 10 * (d - c) < (b - a)
+    res = execute(pstore_dir)
+    t2 = res['time']
+    assert_equal writable_tags, res['writable_tags']
+    assert t2 < 1.0, format('loading cached tag information should be done in under 1 second but needed %.2fs', t2)
+    assert 10 * t2 < t1, format('loading cached tag information (%.2fs) should be 10 times faster than loading uncached information (%.2fs)', t2, t1)
   ensure
     FileUtils.rm_rf pstore_dir
+  end
+
+  private
+
+  def execute pstore_dir
+    script = <<-END
+      MiniExiftool.pstore_dir = '#{pstore_dir}'
+      res = {}
+      start = Time.now
+      res['writable_tags'] = MiniExiftool.writable_tags
+      res['time'] = Time.now - start
+      puts YAML.dump res
+    END
+    cmd = %Q(#{RUBY_ENGINE} -EUTF-8 -I lib -r mini_exiftool -r yaml -e "#{script}")
+    YAML.load(`#{cmd}`)
   end
 
 end
