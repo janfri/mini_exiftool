786113e1ed27cdf390795fd92b78d5ae44bade39
Jan Friedrich <janfri26@gmail.com>
2019-10-28 18:48:16 +0100

    Prepare release 2.9.1

--- lib/mini_exiftool.rb
+++ lib/mini_exiftool.rb
@@ -26,7 +26,7 @@ require 'time'
 # Simple OO access to the ExifTool command-line application.
 class MiniExiftool
 
-  VERSION = '2.9.0'
+  VERSION = '2.9.1'
 
   # Name of the ExifTool command-line application
   @@cmd = 'exiftool'
