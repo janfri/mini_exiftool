afd09c96067e5c64737b4b8682f5d8a0683ce4ce
Jan Friedrich <janfri26@gmail.com>
2024-12-30 20:36:42 +0100

    Prepeare release 2.13.0

--- lib/mini_exiftool.rb
+++ lib/mini_exiftool.rb
@@ -28,7 +28,7 @@ require 'yaml'
 # Simple OO access to the ExifTool command-line application.
 class MiniExiftool
 
-  VERSION = '2.12.1'
+  VERSION = '2.13.0'
 
   # Name of the ExifTool command-line application
   @@cmd = 'exiftool'
